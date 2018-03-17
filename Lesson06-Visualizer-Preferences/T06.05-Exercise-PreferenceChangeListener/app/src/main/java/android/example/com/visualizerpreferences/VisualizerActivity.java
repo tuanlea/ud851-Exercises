456f21f5f883c603414acc5a81f89af2a75c8f82
Tuan Le <tuan.le.a@gmail.com>
2018-03-17 14:00:01 -0700

    Finished Lesson 6.5 Preference change listener

--- Lesson06-Visualizer-Preferences/T06.05-Exercise-PreferenceChangeListener/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
+++ Lesson06-Visualizer-Preferences/T06.05-Exercise-PreferenceChangeListener/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
@@ -34,7 +34,8 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 // TODO (1) Implement OnSharedPreferenceChangeListener
-public class VisualizerActivity extends AppCompatActivity {
+public class VisualizerActivity extends AppCompatActivity
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     private static final int MY_PERMISSION_RECORD_AUDIO_REQUEST_CODE = 88;
     private VisualizerView mVisualizerView;
@@ -59,6 +60,7 @@ public class VisualizerActivity extends AppCompatActivity {
         mVisualizerView.setMinSizeScale(1);
         mVisualizerView.setColor(getString(R.string.pref_color_red_value));
         // TODO (3) Register the listener
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
     }
 
     // TODO (2) Override the onSharedPreferenceChanged method and update the show bass preference
@@ -102,6 +104,13 @@ public class VisualizerActivity extends AppCompatActivity {
         if (mAudioInputReader != null) {
             mAudioInputReader.shutdown(isFinishing());
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .unregisterOnSharedPreferenceChangeListener(this);
     }
 
     @Override
@@ -150,6 +159,14 @@ public class VisualizerActivity extends AppCompatActivity {
             }
             // Other permissions could go down here
 
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(getString(R.string.pref_show_bass_key))) {
+            mVisualizerView.setShowBass(sharedPreferences.getBoolean(key,
+                    getResources().getBoolean(R.bool.pref_show_bass_default)));
         }
     }
 }
